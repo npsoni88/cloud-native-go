d7745b3efdd7005c8ad2a085694b8089e8c9376d
Nishant Soni <nishant.soni@king.com>
2022-10-22 11:56:43 +0200

    GET function and handler

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"io"
 	"log"
 	"net/http"
@@ -10,6 +11,7 @@ import (
 
 // Global variable for key value store
 var store = make(map[string]string)
+var errNoSuchKey = errors.New("no such key")
 
 func myFunc(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Welcome to my simple API "))
@@ -19,11 +21,12 @@ func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/", myFunc)
 	router.HandleFunc("/v1/{key}", keyValuePutHandler).Methods("PUT")
+	router.HandleFunc("/v1/{key}", keyValueGetHandler).Methods("GET")
 	log.Fatal(http.ListenAndServe(":8080", router))
 
 }
 
-/* Put function of the API that must
+/* Put function and handler of the API that must
 - only match PUT requests for /v1/key/{key} path
 - must respond with 201 (created) when a k-v is actually created
 - must respond with 500 (internal error) in case of unexpected errors
@@ -51,4 +54,37 @@ func keyValuePutHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	w.WriteHeader(http.StatusCreated)
 
+}
+
+/* GET function and handler that must
+- only match GET at /v1/key/{key}
+- must call the get function from the API
+- must respond with a 404 when key doesn't exist
+- must respond with the value and a status 200 if the key exists
+- must respond with 500 in case of unexpected errors
+*/
+
+func Get(key string) (string, error) {
+	value, ok := store[key]
+	if !ok {
+		return "", errNoSuchKey
+	}
+	return value, nil
+
+}
+
+func keyValueGetHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	key := vars["key"]
+
+	value, err := Get(key)
+	if errors.Is(err, errNoSuchKey) {
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Write([]byte(value))
 }
