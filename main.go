0f79dfedfc7e02a69b768bea65c25de446c7a5a8
Nishant Soni <nishant.soni@king.com>
2022-10-22 19:12:20 +0200

    DELETE function and handler

--- main.go
+++ main.go
@@ -5,12 +5,17 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"sync"
 
 	"github.com/gorilla/mux"
 )
 
 // Global variable for key value store
-var store = make(map[string]string)
+var store = struct {
+	sync.RWMutex
+	m map[string]string
+}{m: make(map[string]string)}
+
 var errNoSuchKey = errors.New("no such key")
 
 func myFunc(w http.ResponseWriter, r *http.Request) {
@@ -22,18 +27,21 @@ func main() {
 	router.HandleFunc("/", myFunc)
 	router.HandleFunc("/v1/{key}", keyValuePutHandler).Methods("PUT")
 	router.HandleFunc("/v1/{key}", keyValueGetHandler).Methods("GET")
+	router.HandleFunc("/v1/{key}", keyValueDeleteHandler).Methods("DELETE")
 	log.Fatal(http.ListenAndServe(":8080", router))
 
 }
 
 /* Put function and handler of the API that must
-- only match PUT requests for /v1/key/{key} path
+- only match PUT requests for /v1/{key} path
 - must respond with 201 (created) when a k-v is actually created
 - must respond with 500 (internal error) in case of unexpected errors
 */
 
 func Put(key, value string) error {
-	store[key] = value
+	store.Lock()
+	store.m[key] = value
+	store.Unlock()
 	return nil
 }
 
@@ -57,7 +65,7 @@ func keyValuePutHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 /* GET function and handler that must
-- only match GET at /v1/key/{key}
+- only match GET at /v1/{key}
 - must call the get function from the API
 - must respond with a 404 when key doesn't exist
 - must respond with the value and a status 200 if the key exists
@@ -65,7 +73,9 @@ func keyValuePutHandler(w http.ResponseWriter, r *http.Request) {
 */
 
 func Get(key string) (string, error) {
-	value, ok := store[key]
+	store.RLock()
+	value, ok := store.m[key]
+	store.RUnlock()
 	if !ok {
 		return "", errNoSuchKey
 	}
@@ -87,4 +97,31 @@ func keyValueGetHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	w.Write([]byte(value))
+}
+
+/* Delete function and handler that must
+- only match DELETE at /v1/{key}
+- must call the function to Delete
+- respond with 200 status code if deleted
+- respond with internal server error in case of unexpected error
+*/
+
+func Delete(key string) error {
+	store.Lock()
+	delete(store.m, key)
+	store.Unlock()
+	return nil
+}
+
+func keyValueDeleteHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	key := vars["key"]
+
+	err := Delete(key)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
 }
